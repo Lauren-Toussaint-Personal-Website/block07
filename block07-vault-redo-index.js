632f346b0f9c21ec56f0e3f812ba2eae17aa5f7e
Lauren August Toussaint <96217271+la-toussaint@users.noreply.github.com>
2023-07-21 23:38:27 -0400

    Add files via upload

---
+++ block07-vault-redo-index.js
@@ -0,0 +1,11 @@
+const a= 2
+const b= 10
+const c= 4
+const d= 8
+const e= 5
+const num1 = a + d;
+const num2 = b * c;
+const num3 =(d + e) * ((e - a));
+
+
+console.log(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}. We wish you luck opening the lock!`);
